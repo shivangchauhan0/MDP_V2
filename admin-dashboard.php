556e1b96ea47d973d690fab7145927a6a606d43c
Shivang Chauhan <38112760+shivangchauhan0@users.noreply.github.com>
2020-09-29 10:34:53 +0530

    COMMIT

---
+++ admin-dashboard.php
@@ -0,0 +1,13 @@
+<?php include("header.php") ?>
+<?php 
+  if (!isset($_SESSION['username'])) {
+    $_SESSION['msg'] = "You must log in first";
+    header('location: login.php');
+  }
+  if (isset($_GET['logout'])) {
+    session_destroy();
+    unset($_SESSION['username']);
+    header("location: login.php");
+  }
+  ?>
+<?php include("footer.php") ?>
