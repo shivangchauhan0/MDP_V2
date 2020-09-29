34bc2e1520b88ea4aaedd298c86fa352295d6beb
Shivang Chauhan <38112760+shivangchauhan0@users.noreply.github.com>
2020-09-29 12:12:19 +0530

    COMMIT

--- admin-dashboard.php
+++ admin-dashboard.php
@@ -14,12 +14,15 @@
         <h2 class="ml-2 my-0 nav-head"><i class="fa fa-desktop fa-fw" aria-hidden="true"></i> DASHBOARD</h2>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-            <p id="username" class="mt-0 mr-3"><i class='user icon'></i><strong><?php echo $_SESSION['name'] ?></strong></p>
+            <button type="submit" class="ui button tiny bg-red mx-3" id="insert-id">Users</button>
+            <p id="username" class="mt-0 mr-3 mt-2"><i class='user icon'></i><strong><?php echo $_SESSION['name'] ?></strong></p>
           </ul>
         </div>
  </nav>
-
- <form method="post" action="admin-dashboard.php" class="ui form mx-2">
+ <div class="head-bar">
+    <h2 class="my-3 mx-4">ADD NEW ID</h2>
+</div>
+ <form method="post" action="admin-dashboard.php" class="ui form mx-4">
     <?php include('errors.php'); ?>
     <div class="fields">
       <div class="six wide field">
@@ -38,36 +41,38 @@
             </select>
       </div>
       <div class="field">
-		  <button type="submit" class="ui button" id="insert-id" name="insert_tid">Insert</button>
+            <label style="visibility:hidden">Designation</label>
+            <button type="submit" class="ui button bg-red" id="insert-id" name="insert_tid">Insert</button>
 	  </div>
     </div>
 </div>
 </form>
-<div class="container-four mx-2">
-<table class="table id table-bordered">
-  <thead>
-	  <tr id="table-head">
-      <th scope="col" >ID</th>
-      <th scope="col">Designation</th>
-      <th scope="col">Delete</th>
-    </tr>
-  </thead>
-  <tbody>
+<div class="container-four mx-4">
+  <table style="" class="ui celled table">
+        <thead>
+          <tr id="table-head">
+            <th>ID</th>
+            <th>Designation</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
       <?php
 		$sql = "SELECT * FROM `verifyid` WHERE 1";
 		$result = $db-> query($sql);
 		if($result-> num_rows > 0){	
-			while ($row = $result-> fetch_assoc()) {
-                echo "<tr><td>".$row["tid"]."</td><td>".$row["designation"]."</td>
+			while ($row = $result-> fetch_assoc()) { ?>
+                <tr><td><?php echo $row["tid"] ?></td>
+                <td><?php echo $row["designation"] ?></td>
                 <td>
                 <form method='post' action='admin-dashboard.php' class='ui form delete'>
-							 <button onclick='return checkDelete()' type='submit' name='delete_two' value='".$row['id']."' id='delete' class='ui mini icon button delete'>
+							 <button onclick='return checkDelete()' type='submit' name='delete_two' value='<?php $row['id'] ?>' id='delete' class='ui mini icon button delete'>
 								<i class='trash icon'></i>
 							 </button>
                          </form>   
                 </td>
-                </tr>";
-			}
+                </tr>
+		<?php	}
 		}
 	?>
   </tbody>
