24ea65e1b5a4fc3212afc316c74bea0c362c9909
Shivang Chauhan <38112760+shivangchauhan0@users.noreply.github.com>
2020-10-01 17:47:05 +0530

    COMMIT

--- sub-departments.php
+++ sub-departments.php
@@ -6,11 +6,7 @@
         <nav
           class="navbar navbar-expand-lg navbar-light bg-light border-bottom"
         >
-        <?php if (condition) { ?>
-            <a href="index.php?logout='1'"><button class="ui button bg-red small"><i class='sign in alternate icon'></i></i> Logout</button></a> 
-        <?php }else { ?>
         <button class="ui button bg-red small" id="menu-toggle"><i class="fa fa-bars mr-1" aria-hidden="true"></i> Menu</button>
-       <? }?>
         <h2 class="ml-2 my-0 nav-head">CORRECTION</h2>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
