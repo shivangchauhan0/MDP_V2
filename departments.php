f9cfcc2427305bd1fff89afe138af78b45c19e57
Shivang Chauhan <38112760+shivangchauhan0@users.noreply.github.com>
2020-10-10 20:25:52 +0530

    commit

--- departments.php
+++ departments.php
@@ -14,7 +14,7 @@
           </ul>
         </div>
  </nav>
-    <div class="container-fluid my-3">
+ <div class="container-fluid my-3">
         <div class="head-bar">
             <h2>LIST OF DEPARTMENTS</h2>
         </div>
@@ -38,9 +38,43 @@
                     { $srno++; ?>
                         <tr>
                             <td style="width:11vw"><span class="mx-1"><?php echo $srno ?></span></td>
-                            <td><a href="sub-departments.php?sup_depart=<?php echo $row['sup_depart']?>&unchecked=true" style="color:#000" class="mx-1"><?php echo $row["sup_depart"] ?></a></td>
+                            <td><a href="sub-departments.php?sup_depart=<?php echo $row['sup_depart']?>" style="color:#000" class="mx-1"><?php echo $row["sup_depart"] ?></a></td>
                             <td style="width:11vw">
-                                <a href="sub-departments.php?sup_depart=<?php echo $row['sup_depart']?>&unchecked=true"><button type="submit" class="ui button tiny bg-red mx-1" id="insert-id">Correct <i class="fa fa-pen fa-fw ml-1" aria-hidden="true"></i></button></a> 
+                                <a href="sub-departments.php?sup_depart=<?php echo $row['sup_depart']?>"><button type="submit" class="ui button tiny bg-red mx-1" id="insert-id">Correct <i class="fa fa-pen fa-fw ml-1" aria-hidden="true"></i></button></a> 
+                            </td>
+                        </tr>
+                <?php	
+                     }
+                }
+            ?>
+            </tbody>
+        </table>
+    </div>
+ <div class="container-fluid my-3">
+        <div class="head-bar">
+            <h2>LIST OF DEANS</h2>
+        </div>
+        <table class="ui celled table my-4">
+            <thead>
+                <tr id="table-head">
+                <th>Srno</th>
+                <th>Name</th>
+                <th>Correct</th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php
+                $sql = "SELECT * FROM `users` WHERE  `designation`= 'Dean' OR `designation`= 'Vice-Principal' " ;
+                $result = $db-> query($sql);
+                $srno = 0;
+                if($result-> num_rows > 0){	
+                    while ($row = $result-> fetch_assoc())
+                    { $srno++; ?>
+                        <tr>
+                            <td style="width:11vw"><span class="mx-1"><?php echo $srno ?></span></td>
+                            <td><a href="correction.php?id=<?php echo $row['username']?>&unchecked=true" style="color:#000" class="mx-1"><?php echo $row["name"] ?></a></td>
+                            <td style="width:11vw">
+                                <a href="correction.php?id=<?php echo $row['username']?>&unchecked=true"><button type="submit" class="ui button tiny bg-red mx-1" id="insert-id">Correct <i class="fa fa-pen fa-fw ml-1" aria-hidden="true"></i></button></a> 
                             </td>
                         </tr>
                 <?php	
