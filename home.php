bde0a8992b6c1dc83e70017c9e5214a24e8e3fa8
Shivang Chauhan <38112760+shivangchauhan0@users.noreply.github.com>
2020-10-12 12:19:22 +0530

    COMMIT

---
+++ home.php
@@ -0,0 +1,12 @@
+<?php include('header.php') ?>
+<div class="body-container">
+  <div class="content">
+    <h1 id="home-head">WELCOME TO  <br> TEACHER'S DIARY</h1>
+    <p>A digital user interface for teacher's diary</p>
+    <div class="btn-grid mt-4">
+        <a href='login.php' id="record-btn" class='ui button d-inline bg-red'>Login</a>
+        <a href='register.php' id="timetable-btn"  class='ui button d-inline bg-red'>Signup</a>
+     </div>
+  </div>
+</div>
+<?php include('footer.php') ?>
