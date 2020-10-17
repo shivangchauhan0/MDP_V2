29fcaaef30b478253620a93682a2da815429dd09
Shivang Chauhan <38112760+shivangchauhan0@users.noreply.github.com>
2020-10-17 18:42:35 +0530

    COMMIT

--- index.php
+++ index.php
@@ -148,7 +148,7 @@
                           <option value="Monday">Monday</option>
                           <option value="Tuesday">Tuesday</option>
                           <option value="Wednesday">Wednesday</option>
-                          <option value="Thrusday">Thrusday</option>
+                          <option value="Thursday">Thursday</option>
                           <option value="Friday">Friday</option>
                           <option value="Saturday">Saturday</option>
                       </select>
