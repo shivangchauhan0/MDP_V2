029213808a5355c32ac88bc58ea29a2d9cb7daf7
Shivang Chauhan <38112760+shivangchauhan0@users.noreply.github.com>
2021-02-26 20:35:18 +0530

    Uncommented login/logout

--- sidenav-head.php
+++ sidenav-head.php
@@ -62,13 +62,13 @@
       </div>
       <!-- /#sidebar-wrapper -->
 <?php 
-  // if (!isset($_SESSION['username'])) {
-  //   $_SESSION['msg'] = "You must log in first";
-  //   header('location: login.php');
-  // }
-  // if (isset($_GET['logout'])) {
-  //   session_destroy();
-  //   unset($_SESSION['username']);
-  //   header("location: login.php");
-  // }
+  if (!isset($_SESSION['username'])) {
+    $_SESSION['msg'] = "You must log in first";
+    header('location: login.php');
+  }
+  if (isset($_GET['logout'])) {
+    session_destroy();
+    unset($_SESSION['username']);
+    header("location: login.php");
+  }
 ?>
