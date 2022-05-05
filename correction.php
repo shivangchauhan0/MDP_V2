0bd929e416792d908e209004ef95110d9f449f75
Shivang Chauhan <38112760+shivangchauhan0@users.noreply.github.com>
2022-05-05 18:45:06 +0530

    Commit

--- correction.php
+++ correction.php
@@ -69,7 +69,7 @@
             <?php  } ?>
             <a class="float-right" href="correction-detailed.php?id=<?php echo isset($_GET['id']) ? $_GET['id'] : "" ; ?>&unchecked=<?php echo isset($_GET['unchecked']) ? $_GET['unchecked'] : "" ?>"><button type="submit" class="ui button bg-red mx-1 my-2 tiny" id="insert-id">Detailed</button></a> 
             <a class="float-right" href="correction.php?id=<?php echo isset($_GET['id']) ? $_GET['id'] : "" ; ?>&limit=<?php echo isset($_GET['limit']) ? $_GET['limit'] : "25" ?> ?&unchecked=<?php echo isset($_GET['unchecked']) ? $_GET['unchecked'] : "true" ; ?>"><button type="submit" class="ui button bg-red mx-1 my-2 tiny " id="insert-id">Unset Filters</button></a>
-            <a class="float-right" href="check-logs.php"><button type="submit" class="ui button bg-red mx-1 my-2 tiny" id="insert-id">Checking logs</button></a>
+            <a class="float-right" href="correction-logs.php"><button type="submit" class="ui button bg-red mx-1 my-2 tiny" id="insert-id">Checking logs</button></a>
         </div>
         <?php if (isset($_GET['unchecked']) && $_GET['unchecked'] == 'true') { ?>
         <div style="" class="check-form-seg">
